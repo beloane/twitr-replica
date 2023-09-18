ad7d5de0134b625c0ebe046dd725580fba2d6a3e
ioan beloane <ioanbeloane@gmail.com>
2023-09-18 13:17:11 +0300

    added mobile nav

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+////////////////////////////Make mobile navigation//////
+const btnNav = document.querySelector(".mobile-header");
+const lay = document.querySelector(".layout");
+const mobileNavWindow = document.querySelector(".header-container-mobile-nav");
+const mobileNav = document.querySelector(".header-mobile-nav");
+
+mobileNavWindow.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".header-mobile-nav");
+  console.log(clicked);
+  if (!clicked) lay.classList.remove("nav-open");
+});
+
+btnNav.addEventListener("click", function () {
+  lay.classList.add("nav-open");
+});
+//// Making right side sticky
 const targetEl = document.querySelector(".target-int");
 const sectionRightEl = document.querySelector(".controls");
 
